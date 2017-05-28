db396aba5d369f5164faddbf6596f8f3f901e37a
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-05-28 07:59:52 -0400

    Added boat along with bobbing animation

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour {
+
+	[SerializeField] private float bobAmount = 1f;
+	[SerializeField] private float bobSpeed = 1f;
+
+	void Update () {
+		transform.eulerAngles = new Vector3(
+			Mathf.Sin(Time.time * bobSpeed * 1.2f) * bobAmount,
+			transform.eulerAngles.y,
+			Mathf.Sin(Time.time * bobSpeed) * bobAmount
+		);
+	}
+}
