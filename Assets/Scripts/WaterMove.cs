53e9f1107a05e311a788d5e0458e92c6ce37f0b0
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-05-28 03:10:00 -0400

    Animated water!!! wooo

---
+++ Assets/Scripts/WaterMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class WaterMove : MonoBehaviour {
+
+	[SerializeField] private float speed = 0.11f;
+	[SerializeField] private float scale = 46f;
+
+	private Renderer rend;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update() {
+		float scaleX = (Mathf.Cos(Time.time * speed) * 0.5F + 1) * scale;
+		float scaleY = (Mathf.Sin(Time.time * speed) * 0.5F + 1) * scale;
+		rend.material.mainTextureScale = new Vector2(scaleX, scaleY);
+	}
+}
