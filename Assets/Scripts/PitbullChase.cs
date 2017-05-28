db4f9e8e4367a2b15c930f414f975d2b69d14294
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2017-05-28 08:19:36 -0400

    Added pitbull, it chases the car

---
+++ Assets/Scripts/PitbullChase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PitbullChase : MonoBehaviour {
+
+	private NavMeshAgent m_agent;
+	private Transform car;
+
+	void Start () {
+		m_agent = GetComponent<NavMeshAgent>();
+
+		car = CarScoreManager.Instance.transform;
+		m_agent.destination = car.position;
+	}
+
+	void Update () {
+		m_agent.destination = car.position;
+	}
+}
